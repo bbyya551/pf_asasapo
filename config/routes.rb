58d46aea60ab342f1e833300b08e1f5487def577
EC2 Default User <ec2-user@ip-172-31-35-228.ap-northeast-1.compute.internal>
2022-08-03 11:00:19 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,32 @@
+Rails.application.routes.draw do
+
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    get 'top' => 'homes#top', as: 'top'
+    resources :users, only: [:show, :edit, :update, :index]
+    resources :posts, only: [:show, :index, :destroy] do
+      resources :post_comments, only: [:destroy, :index]
+    end
+    resources :groups, only: [:index, :destroy, :show]
+  end
+
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  scope module: :public do
+    root 'homes#top'
+
+    resources :users, only: [:show, :edit, :update, :index]
+    resources :posts do
+      resources :post_comments, only: [:create, :destroy]
+      resource :favorites, only: [:create, :destroy]
+    end
+    resources :groups
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
