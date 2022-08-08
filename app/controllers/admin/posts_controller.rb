8fb4648f9deb892413b02d4e580874c3cc2dd987
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-08-08 15:14:08 +0000

    [Add] admin

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,19 @@
 class Admin::PostsController < ApplicationController
+  before_action :authenticate_admin!
   def show
+    @post = Post.find(params[:id])
+    @post_comment = PostComment.new
+    @post_comments = @post.post_comments
+    @user = @post.user
   end
 
   def index
+    @posts = Post.all
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to admin_posts_path
   end
 end
