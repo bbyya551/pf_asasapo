69672475b15b5e9eda4c0b1a3782d3ad246282a4
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-08-13 07:30:36 +0000

    post_genre_edit

--- db/migrate/20220803101458_create_post_genres.rb
+++ db/migrate/20220803101458_create_post_genres.rb
@@ -1,8 +1,8 @@
 class CreatePostGenres < ActiveRecord::Migration[6.1]
   def change
     create_table :post_genres do |t|
-      t.references :post, null: false, foreign_key: true
-      t.references :genre, null: false, foreign_key: true
+      t.references :post, null: false
+      t.references :genre, null: false
 
       t.timestamps
     end
