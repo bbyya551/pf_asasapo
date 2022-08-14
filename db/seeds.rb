5b52d8681894a2b965b97937dd90919972349525
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-08-14 04:59:57 +0000

    [Add]Admininfo

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(
    email: ENV['ADMIN_EMAIL'],
-   encrypted_password: ENV['ADMIN_PASSWORD']
+   password: ENV['ADMIN_PASSWORD']
 )
