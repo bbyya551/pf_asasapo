e42661f883c87d48c6c15d3461da7cf0f671e669
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-08-14 05:17:19 +0000

    [Add]post vali

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,8 +16,11 @@ class Post < ApplicationRecord
 
   has_many :notifications, dependent: :destroy
 
+  # validates :title, presence:true
+  # validates :body, presence:true,length:{maximum: 180}
+
   validates :title, presence:true
-  validates :body, presence:true,length:{maximum: 180}
+  validates :body, presence:true,length:{maximum: 200}
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
