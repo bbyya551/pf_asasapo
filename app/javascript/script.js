20986191d89f9463b254059fe94579c1764305a9
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-08-20 15:00:27 +0000

    test commit

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -7,49 +7,29 @@
 //     if ( (scrollHeight - scrollPosition) / scrollHeight <= 0.05) {
 //           $('.jscroll').jscroll({
 //             contentSelector: '.scroll-list',
+            // 'a[rel=next]:last'は、htmlのページネーションのclassはどれも同じだった。nextボタンのみ、何か指定してやる必要があり、nextボタンには、rel = nextが指定されていたので、それをNextSelecterとして指定した。
 //             nextSelector: 'a[rel=next]:last'
 //           });
 //     }
 //   });
 // });
-
+// 遷移するとき、差分を読み込んでいるだけなので、この記述(turbolinks:load)がないと動作しない。
 $(document).on('turbolinks:load', function () {
   // console.log("location.href");
   // console.log(location.href.match(/chats/));
   // console.log("location.href");
 
+  // https://mebee.info/2020/11/23/post-17840/
+  // https://uxmilk.jp/9205
+  // https://qiita.com/kazu56/items/557740f398e82fc881df
+  // if ( str.match(/hoge/)) ⇦ここの例で言うと、str = location.hrefを代入してもいい。
+  // location.hrefでurlを取得してきて、その中にchatsが含まれるかと言う意味。
   if ( location.href.match(/chats/)) {
     //strにhogeを含む場合の処理
     let elm = document.documentElement;
+    // scrollHeight ページの高さ clientHeight ブラウザの高さ
     let bottom = elm.scrollHeight - elm.clientHeight;
+    // 垂直方向へ移動window.scroll( 数値(水平) , 数値(垂直) ) ;
     window.scroll(0, bottom);
   }
-
 });
-
-
-//   $(document).ready(function () {
-//     $("#theTarget").skippr({
-//       // スライドショーの変化 ("fade" or "slide")
-//       transition : 'slide',
-//       // 変化に係る時間(ミリ秒)
-//       speed : 1000,
-//       // easingの種類
-//       easing : 'easeOutQuart',
-//       // ナビゲーションの形("block" or "bubble")
-//       navType : 'block',
-//       // 子要素の種類('div' or 'img')
-//       childrenElementType : 'div',
-//       // ナビゲーション矢印の表示(trueで表示)
-//       arrows : true,
-//       // スライドショーの自動再生(falseで自動再生なし)
-//       autoPlay : true,
-//       // 自動再生時のスライド切替間隔(ミリ秒)
-//       autoPlayDuration : 3000,
-//       // キーボードの矢印キーによるスライド送りの設定(trueで有効)
-//       keyboardOnAlways : true,
-//       // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
-//       hidePrevious : false
-//     });
-//   });
-// });
