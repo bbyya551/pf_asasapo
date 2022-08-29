cd89138902bf8789177a5d8c147e56d99f80141b
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-08-29 15:33:18 +0000

    [Edit]activestrage

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,8 @@ class Post < ApplicationRecord
   def get_post_image(width, height)
     unless post_image.attached?
       file_path = Rails.root.join('app/assets/images/post_default.jpg')
-      post_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      # post_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      post_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
     end
     post_image.variant(resize_to_limit: [width, height]).processed
   end
