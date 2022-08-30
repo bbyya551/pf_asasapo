f8aa1896db99409dd8406c2fe0752449a315b3c0
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-08-30 16:23:04 +0000

    [Add]groups_spec

--- spec/system/groups_spec.rb
+++ spec/system/groups_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'コミュニティ一覧画面のテスト' do
   let(:user) { create(:user) }
-  let!(:group) { create(:group, :group_with_users) }
+  let!(:group) { create(:group, :group_with_users, owner_id: user.id) }
   let!(:other_group) { create(:group, :group_with_users) }
 
   before do
@@ -32,6 +32,15 @@ describe 'コミュニティ一覧画面のテスト' do
       it 'コミュニティに参加中のメンバーの人数が表示される' do
         expect(page).to have_content group.group_users.count
         expect(page).to have_content other_group.group_users.count
+      end
+
+      it 'コミュニティのShowリンクが表示される' do
+        expect(page).to have_link 'Show', href: group_path(group)
+      end
+
+      it 'コミュニティ編集画面へのリンクが表示される' do
+        #最初に、 let!(:group) { create(:group, :group_with_users, owner_id: user.id) }で、owner_id: user.idとして、owner_idをログインしているユーザーに指定してやる必要がある!
+        expect(page).to have_link '編集', href: edit_group_path(group)
       end
     end
   end
