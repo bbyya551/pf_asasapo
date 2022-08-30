744a4deaf10c32d7f21a96cae188f4304a85a273
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-08-30 10:33:16 +0000

    [Add]test

--- app/models/group.rb
+++ app/models/group.rb
@@ -9,8 +9,8 @@ class Group < ApplicationRecord
   def get_image(width, height)
    unless image.attached?
      file_path = Rails.root.join('app/assets/images/group_default.jpg')
-    # image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-     image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
+     image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    # image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
    end
   image.variant(resize_to_limit: [width, height]).processed
   end
