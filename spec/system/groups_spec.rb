d0bb591d9eb6d3f6316cc085a63f2e78c1ec37c1
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-09-01 03:34:16 +0000

    [Edit]groups_spec

--- spec/system/groups_spec.rb
+++ spec/system/groups_spec.rb
@@ -98,12 +98,92 @@ describe 'コミュニティ一覧画面のテスト' do
       fill_in 'user[email]', with: other_user.email
       fill_in 'user[password]', with: other_user.password
       click_button 'Log in'
+      # byebug
       visit group_path(group.id)
+      @owner = User.find(group.owner_id)
     end
 
     context '表示の確認' do
       it 'URLが正しい' do
         expect(current_path).to eq '/groups/' + group.id.to_s
+      end
+
+      it 'コミュニティ詳細ページと表示される' do
+        expect(page).to have_content 'コミュニティ詳細ページ'
+      end
+
+      it 'コミュニティの名前が表示される' do
+        expect(page).to have_content group.name
+      end
+
+      it 'コミュニティの説明が表示される' do
+        expect(page).to have_content group.introduction
+      end
+
+      it 'コミュニティオーナー名のリンク先が正しい' do
+        expect(page).to have_link @owner.name, href: user_path(@owner)
+      end
+
+      it 'このコミュニティに参加が表示され、リンク先が正しい' do
+        expect(page).to have_link 'このコミュニティに参加', href: group_group_users_path(group.id)
+        # byebug
+      end
+
+      it 'コミュニティメンバーが一覧で表示され、リンク先が正しい' do
+        group.users.each do |user|
+          expect(page).to have_link user.name, href: user_path(user.id)
+        end
+      end
+    end
+  end
+
+  describe 'コミュニティ詳細画面のテスト(自分がコミュニティのオーナーではなくて、すでにメンバーである時)' do
+    before do
+      logout_link = find_all('a')[6].native.inner_text
+      click_link logout_link
+      expect(current_path).to eq('/')
+      # user = group.users[0]
+      # byebug
+      #すでにグループに存在しているメンバーgroup.users[0]でログインする必要がある
+      visit new_user_session_path
+      fill_in 'user[email]', with: group.users[0].email
+      fill_in 'user[password]', with: group.users[0].password
+      click_button 'Log in'
+      #最初、visit new_user_session_pathの前にuser = User.find(3)と定義してログインしようとしたところ、ログインがそもそもできていなかった。
+      # byebug
+      visit group_path(group.id)
+      @owner = User.find(group.owner_id)
+    end
+
+    context '表示の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/groups/' + group.id.to_s
+      end
+
+      it 'コミュニティ詳細ページと表示される' do
+        expect(page).to have_content 'コミュニティ詳細ページ'
+      end
+
+      it 'コミュニティの名前が表示される' do
+        expect(page).to have_content group.name
+      end
+
+      it 'コミュニティの説明が表示される' do
+        expect(page).to have_content group.introduction
+      end
+
+      it 'コミュニティオーナー名のリンク先が正しい' do
+        expect(page).to have_link @owner.name, href: user_path(@owner)
+      end
+
+      it 'コミュニティから脱退が表示され、リンク先が正しい' do
+        expect(page).to have_link 'コミュニティから脱退', href: group_group_users_path(group.id)
+      end
+
+      it 'コミュニティメンバーが一覧で表示され、リンク先が正しい' do
+        group.users.each do |user|
+          expect(page).to have_link user.name, href: user_path(user.id)
+        end
       end
     end
   end
