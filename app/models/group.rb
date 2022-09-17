f25b4e6f4ef90213f5a55e76ed50b95a90daad66
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-09-17 07:57:11 +0000

    [Edit]rspec

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,6 +6,7 @@ class Group < ApplicationRecord
 
   validates :name, presence: true
   validates :introduction, presence: true
+  validates :owner_id, presence: true
   has_one_attached :image
 
   def get_image(width, height)
