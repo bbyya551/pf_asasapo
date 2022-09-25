8e76b55e23baef212d1be1482fee8f6162fd9f65
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-09-25 10:16:23 +0000

    [Add]chats_sort

--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -3,10 +3,21 @@ class Public::RoomsController < ApplicationController
 
   def index
     @user = current_user
-    @rooms = @user.rooms.joins(:chats).includes(:chats).order("chats.created_at DESC")
+    #joinsはテーブル同士を結合してくれる。left_joinsは外部結合(nilでもnilとしてテーブルを結合してくれる)。joinsは内部結合。
+    #byebug
+    #@rooms = @user.rooms.left_joins(:chats).includes(:chats).group("chats.room_id").order("chats.created_at DESC")
+    #@rooms = @user.rooms.left_joins(:chats).includes(:chats).group("chats.user_id, chats.room_id").order("chats.created_at DESC")
+
+    #@rooms = @user.rooms.joins(:chats).includes(:chats).order("chats.created_at DESC")
+    #idでグルーピングしてやる
+    @rooms = @user.rooms.left_joins(:chats).group(:id).includes(:chats).order("chats.created_at DESC")
+    #@rooms = @user.rooms.left_joins(:chats).includes(:chats).order("chats.created_at DESC")
+    # @rooms = @user.rooms
   end
 
-  # def create
+
+
+    # def create
   #   @room = Room.create
   #   @user_room1 = UserRoom.create(room_id: @room.id, user_id: current_user.id)
   #   @user_room2 = UserRoom.create(params.require(:user_room).permit(:user_id, :room_id).merge(room_id: @room.id))
