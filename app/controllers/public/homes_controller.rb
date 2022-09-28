ec029cd3d375c0251502c214d19c8fe2c1acebaa
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-09-28 13:01:20 +0000

    [Edit]

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
-    @posts = Post.page(params[:page]).order(created_at: :desc).limit(3)
+    @posts = Post.page(params[:page]).status_public.order(created_at: :desc).limit(3)
     @groups = Group.page(params[:page]).order(created_at: :desc).limit(3)
   end
 end
