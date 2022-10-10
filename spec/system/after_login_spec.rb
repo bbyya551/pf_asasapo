e2b88fd51432d6c6266f2b4c08454a85ee01f904
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-10-10 14:55:10 +0000

    ログイン画面日本語化

--- spec/system/after_login_spec.rb
+++ spec/system/after_login_spec.rb
@@ -21,7 +21,7 @@ describe 'ユーザーログイン後のテスト' do
     visit new_user_session_path
     fill_in 'user[email]', with: user.email
     fill_in 'user[password]', with: user.password
-    click_button 'Log in'
+    click_button 'ログイン'
   end
 
   describe 'ヘッダーのテスト: ログインしている場合' do
