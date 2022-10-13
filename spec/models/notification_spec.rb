5adea227071c5a53278b1020061f6922f50f6cf0
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-10-13 15:34:38 +0000

    [Edit]notification_spec.rb

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,8 +1,26 @@
 require 'rails_helper'
 
-Rspec.describe Notification, type: :model do
+RSpec.describe Notification, type: :model do
   describe "create" do
     context "post_comment関連の通知" do
+      before do
+        @post = FactoryBot.build(:post)
+        @post_comment = FactoryBot.build(:post_comment)
+      end
+
+      it "コメントが行われた場合に保存できる" do
+        notification = FactoryBot.build(:notification, post_comment_id: @post_comment.id, action: "comment")
+        expect(notification).to be_valid
+      end
+    end
+
+    context "フォロー関連の通知" do
+      it "フォローが行われた場合に保存できる" do
+        user1 = FactoryBot.build(:user)
+        user2 = FactoryBot.build(:user)
+        notification = FactoryBot.build(:notification, visitor_id: user1.id, visited_id: user2.id, action: "follow")
+        expect(notification).to be_valid
+      end
     end
   end
 end
