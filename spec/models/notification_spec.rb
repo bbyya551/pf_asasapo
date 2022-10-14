80fe051329cf668ea940df86acade353955fd0b3
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-10-14 13:34:24 +0000

    [Edit]notification_spec.rb

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
   describe "create" do
-    context "post_comment関連の通知" do
+    context "post関連の通知" do
       before do
         @post = FactoryBot.build(:post)
         @post_comment = FactoryBot.build(:post_comment)
@@ -12,6 +12,11 @@ RSpec.describe Notification, type: :model do
         notification = FactoryBot.build(:notification, post_comment_id: @post_comment.id, action: "comment")
         expect(notification).to be_valid
       end
+
+      it "いいねされた場合に保存できる" do
+        notification = FactoryBot.build(:notification, post_id: @post.id, action: "Like")
+        expect(notification).to be_valid
+      end
     end
 
     context "フォロー関連の通知" do
@@ -19,6 +24,14 @@ RSpec.describe Notification, type: :model do
         user1 = FactoryBot.build(:user)
         user2 = FactoryBot.build(:user)
         notification = FactoryBot.build(:notification, visitor_id: user1.id, visited_id: user2.id, action: "follow")
+        expect(notification).to be_valid
+      end
+    end
+
+    context "朝活宣言の通知" do
+      it "朝活宣言が行われた場合に保存できる" do
+        @announcement = FactoryBot.build(:announcement)
+        notification = FactoryBot.build(:notification, announcement_id: @announcement.id, action: "announcement")
         expect(notification).to be_valid
       end
     end
