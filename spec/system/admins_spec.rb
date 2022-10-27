89306ca7b38e76731928e5d6e77a9c0d1a1dc9fb
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-10-27 13:45:42 +0000

    [Edit]admins_spec.rb

--- spec/system/admins_spec.rb
+++ spec/system/admins_spec.rb
@@ -13,5 +13,33 @@ describe '管理者ログイン後のテスト' do
     visit new_admin_session_path
     fill_in 'admin[email]', with: admin.email
     fill_in 'admin[password]', with: admin.password
+    click_button 'ログイン'
+  end
+
+  describe 'ヘッダーのテスト : ログインしている場合' do
+    context 'リンクの内容を確認' do
+      it '投稿一覧を押すと、投稿一覧画面に遷移する' do
+        click_link("投稿一覧")
+        expect(current_path).to eq('/admin/posts')
+        # posts_link = find_all('a')[1].native.inner_text
+        # click_link posts_link
+        # expect(current_path).to eq('/admin/posts')
+      end
+
+      it 'コミュニティ一覧を押すと、コミュニティ一覧画面に遷移する' do
+        click_link('コミュニティ一覧')
+        expect(current_path).to eq('/admin/groups')
+      end
+
+      it '会員一覧を押すと、会員一覧画面に遷移する' do
+        click_link('会員一覧')
+        expect(current_path).to eq('/admin/users')
+      end
+
+      it '通報一覧を押すと、通報一覧画面に遷移する' do
+        click_link('通報一覧')
+        expect(current_path).to eq('/admin/reports')
+      end
+    end
   end
 end
