6c8a2eae78f2e9851c3253bbdc0b632f04aa769b
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-10-28 13:51:31 +0000

    [Edit]admins_spec

--- spec/system/admins_spec.rb
+++ spec/system/admins_spec.rb
@@ -4,10 +4,11 @@ describe '管理者ログイン後のテスト' do
   let(:admin) { create(:admin) }
   let!(:user) { create(:user) }
   let!(:other_user) { create(:user) }
-  let!(:post) { create(:post) }
-  let!(:other_post) { create(:post) }
+  let!(:post) { create(:post, user: user) }
+  let!(:other_post) { create(:post, user: other_user) }
   let!(:group) { create(:group) }
   let!(:other_group) { create(:group) }
+  let!(:post_comment) { create(:post_comment, post: post, user: user) }
 
   before do
     visit new_admin_session_path
@@ -39,6 +40,38 @@ describe '管理者ログイン後のテスト' do
       it '通報一覧を押すと、通報一覧画面に遷移する' do
         click_link('通報一覧')
         expect(current_path).to eq('/admin/reports')
+      end
+    end
+  end
+
+  describe '投稿一覧画面のテスト' do
+    before do
+      visit admin_posts_path
+    end
+
+    context '表示内容の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/admin/posts'
+      end
+
+      it '投稿のタイトルのリンク先がそれぞれ正しい' do
+        expect(page).to have_link post.title, href: admin_post_path(post.id)
+        expect(page).to have_link other_post.title, href: admin_post_path(other_post.id)
+      end
+
+      it '投稿の本文が表示される' do
+        expect(page).to have_content post.body.truncate(10)
+        expect(page).to have_content other_post.body.truncate(10)
+      end
+
+      it '投稿に、投稿日が表示される' do
+        expect(page).to have_content post.created_at.strftime('%Y/%m/%d')
+        expect(page).to have_content other_post.created_at.strftime('%Y/%m/%d')
+      end
+
+      it '投稿に、コメント数が表示される' do
+        expect(page).to have_content post.post_comments.count
+        expect(page).to have_content other_post.post_comments.count
       end
     end
   end
